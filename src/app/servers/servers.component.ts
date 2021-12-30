78eeadc99b07e9c9b1d6511a3e95354eff372c71
ahmad <engAhmad@users.noreply.github.com>
2021-12-30 09:48:09 +0200

    binding
    binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "No Server Created!";
+  serverName = "";
 
   constructor() {
     setTimeout(() => {
@@ -19,6 +20,9 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = "Server was Created!";
+    this.serverCreationStatus = "Server was Created! Name is " + this.serverName;
+  }
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
